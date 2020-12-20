ddde79495d718a9450a20cb07eea4d02753b58b4
SysError99 <17522480+SysError99@users.noreply.github.com>
2020-12-20 22:05:18 +0700

    Result: data can be more than 1 type, fix import function

--- src/model/result.js
+++ src/model/result.js
@@ -19,7 +19,7 @@ const Result = function(data){
      */
     this.import = function(d){
         if(typeof d !== 'object') return
-        if(typeof d.data === 'object') _this.data = d.data
+        if(typeof d.data !== 'undefined') _this.data = d.data
         if(typeof d.message === 'string') _this.message = d.message
         if(typeof d.success === 'boolean') _this.success = d.success
     }
