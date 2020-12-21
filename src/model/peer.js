08d1cdee65b89a43bbe794030284e8ffb7aabd8a
SysError99 <17522480+SysError99@users.noreply.github.com>
2020-12-22 02:01:11 +0700

    Peer: will not import any techinal parts with import()

--- src/model/peer.js
+++ src/model/peer.js
@@ -30,13 +30,6 @@ const Peer = function(data){
         if(typeof d.ip === 'string') _this.ip = d.ip
         if(typeof d.port === 'number') _this.port = d.port
         if(typeof d.pub === 'string') _this.pub = d.pub
-        if(typeof d.key === 'object') {
-            if(d.key.isSymmetricKey)
-                _this.key = d.key
-            else
-                _this.key = new SymmetricKey(d.key)
-        }
-        if(typeof d.socket === 'object') _this.socket = d.socket
     }
     /**
      * Export to JSON
