ed95f1163c3200f7cb928a6c17e922778b5802d2
SysError99 <17522480+SysError99@users.noreply.github.com>
2020-12-25 01:47:09 +0700

    Crypt: reduce asymmetric key size

--- src/crypt.js
+++ src/crypt.js
@@ -2,8 +2,41 @@
  * Encrpytion module, separeted from Crpyto for the case of encryption method changes.
  */
 const Crypto = require('crypto')
+/** @type {number} Public key length per row*/
+const len = 64
 /** @type {string} Asymmetric key signing method*/
 const signMethod = 'rsa-sha256'
+/**
+ * Restore public key to its original state
+ * @param {string} str String to be restored
+ * @param {[string,string]} prefix What to be inserted 
+ * @returns {string} Restored string
+ */
+const long = function(str,prefix){
+    let s
+    let strRows = Math.ceil(str.length / len)
+    let strArr = Array(strRows + 2)
+    let strOffset = 0
+    strArr[0] = prefix[0]
+    for(s=1; s <= strRows; s++){
+        strArr[s] = str.substr(strOffset, len)
+        strOffset += len
+    }
+    strArr[s] = prefix[1]
+    return strArr.join('\n')
+}
+/**
+ * Shorten public key 
+ * @param {string} str String to be shortened
+ * @returns {string} 
+ */
+const short = function(str){
+    let strExtract = str.split('\n')
+    strExtract.splice(strExtract.length - 2, 2)
+    strExtract.splice(0,1)
+    return strExtract.join('')
+}
+
 /** Key creator*/
 const keyCreator = {
     /**
@@ -12,7 +45,7 @@ const keyCreator = {
      * @returns {import('crypto').KeyPairSyncResult<string,string>} Key result
      */
     asymmetric: function(password){
-        return Crypto.generateKeyPairSync('rsa', {
+        let k = Crypto.generateKeyPairSync('rsa', {
             modulusLength: 4096,
             namedCurve: 'secp256k1', 
             publicKeyEncoding: {
@@ -26,10 +59,13 @@ const keyCreator = {
                 passphrase: password
             } 
         })
+        k.privateKey = short(k.privateKey)
+        k.publicKey = short(k.publicKey)
+        return k
     },
     /**
      * Generate a new symmetric key
-     * @returns {Buffer[]} Length (2) Key and IV buffer
+     * @returns {[Buffer,Buffer]} Key and IV buffer
      */
     symmetric: function(){
         return [Crypto.randomBytes(32), Crypto.randomBytes(16)]
@@ -37,6 +73,8 @@ const keyCreator = {
 }
 /** Private key encryption functions*/
 const private = {
+    /** @type {[string,string]} Header to be used for long()*/
+    header: ['-----BEGIN ENCRYPTED PRIVATE KEY-----', '-----END ENCRYPTED PRIVATE KEY-----'],
     /**
      * Encrypt with private key
      * @param {string} str String that will be encrypted
@@ -48,7 +86,7 @@ const private = {
         if(typeof str !== 'string' || typeof key !== 'string') return ''
         if(str === '' || key === '') return ''
         return Crypto.privateEncrypt({
-            key: key,
+            key: long(key, private.header),
             passphrase: (typeof password === 'string') ? password : ''
         }, Buffer.from(str, 'utf-8')).toString('base64')
     },
@@ -63,7 +101,7 @@ const private = {
         if(typeof str !== 'string' || typeof key !== 'string') return ''
         if(str === '' || key === '') return ''
         return Crypto.privateDecrypt({
-            key: key,
+            key: long(key, private.header),
             passphrase: (typeof password === 'string') ? password : ''
         }, Buffer.from(str, 'base64')).toString('utf8')
     },
@@ -76,11 +114,13 @@ const private = {
     sign: function(str, key, password){
         if(typeof str !== 'string' || typeof key !== 'string') return ''
         if(str === '' || key === '') return ''
-        return Crypto.sign(signMethod, Buffer.from(str), {key: key, passphrase: password}).toString('base64')
+        return Crypto.sign(signMethod, Buffer.from(str), {key: long(key, private.header), passphrase: password}).toString('base64')
     }
 }
 /** Public key encryption functions*/
 const public = {
+    /** @type {[string,string]} Header to be used for long()*/
+    header: ['-----BEGIN PUBLIC KEY-----', '-----END PUBLIC KEY-----'],
     /**
      * Encrypt with public key
      * @param {string} str String that will be encrypted
@@ -90,7 +130,7 @@ const public = {
     encrpyt: function(str, key){
         if(typeof str !== 'string' || typeof key !== 'string') return ''
         if(str === '' || key === '') return ''
-        return Crypto.publicEncrypt(key, Buffer.from(str,'utf8')).toString('base64')
+        return Crypto.publicEncrypt(long(key, public.header), Buffer.from(str,'utf8')).toString('base64')
     },
     /**
      * Decrypt with public key
@@ -101,7 +141,7 @@ const public = {
     decrypt: function(str, key){
         if(typeof str !== 'string' || typeof key !== 'string') return ''
         if(str === '' || key === '') return ''
-        return Crypto.publicDecrypt(key, Buffer.from(str, 'base64')).toString('utf8')
+        return Crypto.publicDecrypt(long(key, public.header), Buffer.from(str, 'base64')).toString('utf8')
     },
     /**
      * Verify signature of a string with a public key
@@ -113,7 +153,7 @@ const public = {
     verify: function(str, key, signature){
         if(typeof str !== 'string' || typeof key !== 'string' || typeof signature !== 'string') return false
         if(str === '' || key === '' || signature === '') return false
-        return Crypto.verify(signMethod, Buffer.from(str), key, Buffer.from(signature, 'base64'))
+        return Crypto.verify(signMethod, Buffer.from(str), long(key, public.header), Buffer.from(signature, 'base64'))
     }
 }
 /** Symmetric encryption functions*/
