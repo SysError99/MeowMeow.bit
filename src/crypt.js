1e310e9291bc3b3e4243192f86c4a3ada327c04e
SysError99 <17522480+SysError99@users.noreply.github.com>
2020-12-26 04:46:19 +0700

    SignKey: added

--- src/crypt.js
+++ src/crypt.js
@@ -4,8 +4,6 @@
 const Crypto = require('crypto')
 /** @type {number} Public key length per row*/
 const len = 64
-/** @type {string} Asymmetric key signing method*/
-const signMethod = 'rsa-sha256'
 /**
  * Restore public key to its original state
  * @param {string} str String to be restored
@@ -47,7 +45,7 @@ const keyCreator = {
     asymmetric: function(password){
         let k = Crypto.generateKeyPairSync('rsa', {
             modulusLength: 4096,
-            namedCurve: 'secp256k1', 
+            namedCurve: 'secp521r1', 
             publicKeyEncoding: {
                 type: 'spki',
                 format: 'pem'     
@@ -56,8 +54,31 @@ const keyCreator = {
                 type: 'pkcs8',
                 format: 'pem',
                 cipher: 'aes-256-cbc',
-                passphrase: password
+                passphrase: typeof password === 'string' ? password : ''
             } 
+        })
+        k.privateKey = short(k.privateKey)
+        k.publicKey = short(k.publicKey)
+        return k
+    },
+    /**
+     * Generate a new signing key
+     * @returns {import('crypto').KeyPairSyncResult<string,string>} Key result
+     */
+    sign: function(password){
+        let k = Crypto.generateKeyPairSync('ed25519', {
+            modulusLength: 512,
+            namedCurve: 'secp521r1', 
+            publicKeyEncoding: {
+                type: 'spki',
+                format: 'pem'     
+            },
+            privateKeyEncoding: {
+                type: 'pkcs8',
+                format: 'pem',
+                cipher: 'aes-256-cbc',
+                passphrase: typeof password === 'string' ? password : ''
+            }
         })
         k.privateKey = short(k.privateKey)
         k.publicKey = short(k.publicKey)
@@ -104,17 +125,6 @@ const private = {
             key: long(key, private.header),
             passphrase: (typeof password === 'string') ? password : ''
         }, Buffer.from(str, 'base64')).toString('utf8')
-    },
-    /**
-     * Sign a string with a private key
-     * @param {string} str String to be signed
-     * @param {string} key Private key to be signed
-     * @param {string} password Private key password (passphrase)
-     */
-    sign: function(str, key, password){
-        if(typeof str !== 'string' || typeof key !== 'string') return ''
-        if(str === '' || key === '') return ''
-        return Crypto.sign(signMethod, Buffer.from(str), {key: long(key, private.header), passphrase: password}).toString('base64')
     }
 }
 /** Public key encryption functions*/
@@ -142,18 +152,33 @@ const public = {
         if(typeof str !== 'string' || typeof key !== 'string') return ''
         if(str === '' || key === '') return ''
         return Crypto.publicDecrypt(long(key, public.header), Buffer.from(str, 'base64')).toString('utf8')
+    }
+}
+/** Key signing functions*/
+const sign = {
+    /**
+     * Perform a key signing
+     * @param {string} str String to be signed
+     * @param {string} key Key to be used
+     * @param {string} password Password (passphrase) to be used
+     * @returns {string} Signature
+     */
+    perform: function(str,key,password){
+        if(typeof str !== 'string' || typeof key !== 'string') return ''
+        if(str === '' || key === '') return ''
+        return Crypto.sign(null, Buffer.from(str), {key: long(key, private.header), passphrase: password}).toString('base64')
     },
     /**
-     * Verify signature of a string with a public key
-     * @param {string} str String of data to be verified
-     * @param {string} key Public key to be used for verification
+     * Perform key verification
+     * @param {string} str String to be verified
+     * @param {string} key Key to be used
      * @param {string} signature Signature to be verified
-     * @returns {boolean} Is this a correct signature?
+     * @returns {boolean} Is this legit?
      */
-    verify: function(str, key, signature){
+    verify: function(str,key,signature){
         if(typeof str !== 'string' || typeof key !== 'string' || typeof signature !== 'string') return false
         if(str === '' || key === '' || signature === '') return false
-        return Crypto.verify(signMethod, Buffer.from(str), long(key, public.header), Buffer.from(signature, 'base64'))
+        return Crypto.verify(null, Buffer.from(str), long(key, public.header), Buffer.from(signature, 'base64'))
     }
 }
 /** Symmetric encryption functions*/
@@ -191,5 +216,6 @@ module.exports = {
     newKey: keyCreator,
     private: private,
     public: public,
+    sign: sign,
     symmetric: symmetric
 }
---
+++ src/model/key.sign.js
@@ -0,0 +1,77 @@
+const Crypt = require('../crypt')
+/**
+ * Sign key object.
+ * @param {Object|string} d JSON or string for new key (passphrase)
+ */
+const SignKey = function(d){
+    /** This object*/
+    let _this = this
+    /** @type {boolean} This is 'SignKey' object*/
+    this.isSignKey = true
+    /**
+     * Sign a key using private key
+     * @param {string} str String to be signed
+     * @returns {string} Base64-based signature
+     */
+    this.sign = function(str){
+        if(_this.private === '') return ''
+        return Crypt.sign.perform(str, _this.private, _this.password)
+    }
+    /**
+     * Verify signature using public key
+     * @param {string} str String to be verified
+     * @param {string} signature Signature to be verified
+     * @returns {boolean} Is this legit?
+     */
+    this.verify = function(str, signature){
+        if(_this.public === '') return false
+        return Crypt.sign.verify(str, _this.public, signature)
+    }
+    /** @type {string} Saved password*/
+    this.password = ''
+    /** @type {string} Private key*/
+    this.private = ''
+    /** @type {string} Public key*/
+    this.public = ''
+    /**
+     * Generate a new key
+     * @param {string} password Passphrase for this key
+     */
+    let _newKey = function(password){
+        let newKey = Crypt.newKey.sign(password)
+        _this.password = password
+        _this.private = newKey.privateKey
+        _this.public = newKey.publicKey
+    }
+    /**
+     * Import JSON
+     */
+    let _import = function(){
+        if(typeof d.password === 'string') _this.password = d.password
+        if(typeof d.private === 'string') _this.private = d.private
+        if(typeof d.public === 'string') _this.public = d.public
+    }
+    /**
+     * Export to JSON
+     * @returns {Object} JSON
+     */
+    this.export = function(){
+        return {
+            password: _this.password,
+            private: _this.private,
+            public: _this.public
+        }
+    }
+    /**
+     * Export only public key to JSON
+     */
+    this.exportPub = function(){
+        return {
+            public: _this.public
+        }
+    }
+    if(typeof d === 'object') _import()
+    else if(typeof d === 'string') _newKey(d)
+    else _newKey('')
+}
+module.exports = SignKey
