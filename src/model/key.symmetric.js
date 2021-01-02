296f2f3913ba7e4119e741f5b5259200dc696b77
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-01-02 07:53:50 +0700

    SymmetricKey.key: can no longer be accessed directly

--- src/model/key.symmetric.js
+++ src/model/key.symmetric.js
@@ -5,19 +5,17 @@ const Crypt = require('../crypt')
  * @param {string|Object} d JSON or string
  */
 const SymmetricKey = function(d){
-    /** This object*/
-    let _this = this
     /** @type {boolean} This is 'SymmetricKey' object*/
     this.isSymmetricKey = true
     /** @type {Buffer} Key buffer*/
-    this.key = null
+    let key = null
     /**
      * Encrypt a string
      * @param {string} str String to be encrpyted
      * @returns {string} Encrypted string
      */
     this.encrypt = function(str){
-        return Crypt.symmetric.encrypt(str, _this.key)
+        return Crypt.symmetric.encrypt(str, key)
     }
     /**
      * Decrypt a string
@@ -25,13 +23,13 @@ const SymmetricKey = function(d){
      * @returns {string} Decrypted string
      */
     this.decrypt = function(str){
-        return Crypt.symmetric.decrypt(str, _this.key)
+        return Crypt.symmetric.decrypt(str, key)
     }
     /**
      * Create a new key for this object
      */
     let _new = function(){
-        _this.key = Crypt.newKey.symmetric()
+        key = Crypt.newKey.symmetric()
     }
     /**
      * Import JSON
@@ -39,7 +37,7 @@ const SymmetricKey = function(d){
     let _import = function(){
         if(typeof d.key === 'string'){
             try{
-                _this.key = Buffer.from(d.key, 'base64')
+                key = Buffer.from(d.key, 'base64')
             }catch(e){
                 console.error('E -> SymmetricKey.import: importing key: ' + e)
             }
@@ -51,7 +49,7 @@ const SymmetricKey = function(d){
      */
     this.export = function(){
         return {
-            key: _this.key.toString('base64')
+            key: key.toString('base64')
         }
     }
     if(isAny(d)) _import()
