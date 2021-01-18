e38ef49c9a1acd0ea3594776308b44f92b65a788
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-01-19 02:57:37 +0700

    Peer: added last access logging

--- src/model/peer.js
+++ src/model/peer.js
@@ -1,4 +1,5 @@
-const Net = require('net')
+const Try = require('../try.catch')
+const Datagram = require('dgram')
 const SymmetricKey = require('./key.symmetric')
 /** 
  * Peer object
@@ -11,6 +12,8 @@ const Peer = function(d){
     this.isPeer = true
     /** @type {string} Peer IP address*/
     this.ip = ''
+    /** @type {string} Last accessed time*/
+    this.lastAccess = new Date().toUTCString()
     /** @type {number} Peer connected port*/
     this.port = 8080
     /** @type {Buffer} Peer public key.*/
@@ -19,7 +22,7 @@ const Peer = function(d){
     this.key = null
     /** @type {number} Peer quality indicator*/
     this.quality = 5
-    /** @type {Net.Socket} Network socket*/
+    /** @type {Datagram.Socket} Network socket*/
     this.socket = null
     /**
      * Import JSON
@@ -27,7 +30,8 @@ const Peer = function(d){
     let _import = () => {
         if(typeof d[0] === 'string') _.ip = d[0]
         if(typeof d[1] === 'number') _.port = d[1]
-        if(typeof d[2] === 'string') _.pub = Buffer.from(d[2], 'base64')
+        if(typeof d[2] === 'string') _.pub = Try(() => Buffer.from(d[2], 'base64'), _.pub)
+        if(typeof d[3] === 'string') _.lastAccess = d[3]
     }
     /**
      * Export to JSON
@@ -37,7 +41,8 @@ const Peer = function(d){
         return [
             _.ip,
             _.port,
-            _.pub.toString('base64')
+            _.pub.toString('base64'),
+            _.lastAccess
         ]
     }
     if(Array.isArray(d)) _import()
