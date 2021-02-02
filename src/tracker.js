5fe4ce9ddb2e73a639dfc2a3270f15d7bb647c8b
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-02-03 02:28:30 +0700

    Tracker: incomplete foundations

--- src/tracker.js
+++ src/tracker.js
@@ -3,8 +3,8 @@
  * Tracker Server
  */
 const Datagram = require('dgram')
-const Net = require('net')
 
+const __ = require('./const')
 const Try = require('./try.catch')
 const Announcement = require('./model/announcement')
 const ECDHkey = require('./model/key.ecdh')
@@ -16,7 +16,6 @@ const announcement = {}
 const knownPeers = {}
 const myKey = Try(() => new ECDHkey(storage.read('key.server')))
 const storage = require('./storage')(new Locale())
-const udp = Datagram.createSocket('udp4')
 
 /**
  * Shows error via log
@@ -24,23 +23,13 @@ const udp = Datagram.createSocket('udp4')
  */
 const error = err => err ? console.error(err) : 0
 
-Net.createServer({allowHalfOpen: true}, socket => {
-    socket.setEncoding('utf-8')
-    socket.on('error', error)
-    socket.on('data', data => {
-
-    })
-    socket.on('end', () => socket.destroy())
-}).listen(23456)
-
+const udp = Datagram.createSocket('udp4')
 udp.bind(12345)
-udp.on('listening', () => {
-    console.log(`Server is running on port `+udp.address().port)
-})
+udp.on('listening', () => console.log(`Server is running on port `+udp.address().port))
 udp.on('error', error)
 udp.on('message', (msg, remote) => {
-    let message = msg.toString('utf-8')
     let remoteAddress = `${remote.address}:${remote.port}`
+    
     if(typeof announcement[remoteAddress] === 'object'){
         /** @type {Announcement} */
         let ann = announcement[remoteAddress]
@@ -51,33 +40,59 @@ udp.on('message', (msg, remote) => {
         udp.send(payload2, 0, payload2.length, ann.request.port, ann.request.address, error)
         console.log(`Announce ${ann.request.address}:${ann.request.port} -> ${remoteAddress}`)
     }
-    if(message.length > 0){
-        /** @type {Peer} */
-        let peer = knownPeers[remoteAddress]
-        if(typeof peer === 'undefined') return Try(() => {
-            let peer = new Peer([
-                remote.ip,
-                remote.port,
-                message,
-                new Date().toUTCString()
-            ])
-            peer.key = myKey.computeSecret(message)
-            knownPeers[remoteAddress] = peer
-        })
-        message = peer.key.decrypt(message)
-        if(message === '?') {
-            let remotePort = peer.key.decrypt(`${remote.port}`)
-            return udp.send(remotePort, 0, remotePort.length, remote.port, remote.address, error)
+
+    if(msg.length === 0) return
+    
+    /** @type {string|string[]} */
+    let message
+    /** @type {Peer} */
+    let peer
+
+    let identifyPeer = reset => {
+        if(reset) delete knownPeers[remoteAddress]
+        if(typeof knownPeers[remoteAddress] === 'undefined'){
+            peer = new Peer([
+                    remote.ip,
+                    remote.port,
+                    msg
+                ])
+            if(peer.key !== null) knownPeers[remoteAddress] = peer
+            return true
         }
-        if(!IpRegex.test(message)) return
-        if(typeof announcement[message] !== 'object')
-            announcement[message] = new Announcement()
-        /** @type {Announcement} */
-        let ann = announcement[message]
-        ann.request.address = remote.address
-        ann.request.port = remote.port
-        console.log(`Request ${remoteAddress} -> ${message}`)
+        if(new Date() - peer.lastAccess > __.LAST_ACCESS_LIMIT) return identifyPeer(true)
+        peer = knownPeers[remoteAddress]
+    }
+
+    if(Try(identifyPeer)) return
+
+    message = peer.key.decrypt(msg)
+
+    //Announcer
+    switch(message[0]){
+        case '?':
+            let remotePort = peer.key.encrypt(`${remote.port}`)
+            udp.send(remotePort, 0, remotePort.length, remote.port, remote.address, error)
+            return
+        case '>':
+            message = message.slice(0,1)
+            if(!IpRegex.test(message)) return
+            if(typeof announcement[message] !== 'object')
+                announcement[message] = new Announcement()
+            /** @type {Announcement} */
+            let ann = announcement[message]
+            ann.request.address = remote.address
+            ann.request.port = remote.port
+            console.log(`Request ${remoteAddress} -> ${message}`)
+            return
+        default:
+            message = Try(() => JSON.parse(message), null)
+            if(!Array.isArray(message)) return identifyPeer(true)
+    }
+
+    //Tracker
+    switch(message[0]){
+        
     }
 })
 
-console.log(`Public key: ${myKey.get.pub().toString('base64')}`)
+console.log(`Tracker is now on! \n\nPublic key: <${myKey.get.pub().toString('base64')}>`)
