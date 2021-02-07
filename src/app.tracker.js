f22e4b6cca00c2c36702a621804c4562c66081ab
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-02-08 00:46:23 +0700

    Tracker: Make an announcement if the connection is ready

--- src/app.tracker.js
+++ src/app.tracker.js
@@ -24,23 +24,35 @@ const storage = require('./fn.storage')(new Locale())
  */
 const error = err => err ? console.error(err) : 0
 
+/**
+ * Datagram object
+ */
 const udp = Datagram.createSocket('udp4')
+
+/**
+ * Make an announcement to both peers
+ * @param {Datagram.RemoteInfo} remote Remote address of target
+ */
+const announce = remote => {
+    let remoteAddress = `${remote.address}:${remote.port}`
+    /** @type {Announcement} */
+    let ann = announcement[remoteAddress]
+    let payload  = `*${ann.request.address}:${ann.request.port}`
+    let payload2 = `*${remoteAddress}`
+    delete announcement[remoteAddress]
+    udp.send(payload, 0, payload.length, remote.port, remote.address, error)
+    udp.send(payload2, 0, payload2.length, ann.request.port, ann.request.address, error)
+    console.log(`Announce ${ann.request.address}:${ann.request.port} -> ${remoteAddress}`)
+}
+
 udp.bind(12345)
 udp.on('listening', () => console.log(`Server is running on port `+udp.address().port))
 udp.on('error', error)
 udp.on('message', (msg, remote) => {
     let remoteAddress = `${remote.address}:${remote.port}`
     
-    if(typeof announcement[remoteAddress] === 'object'){
-        /** @type {Announcement} */
-        let ann = announcement[remoteAddress]
-        let payload  = `*${ann.request.address}:${ann.request.port}`
-        let payload2 = `*${remoteAddress}`
-        delete announcement[remoteAddress]
-        udp.send(payload, 0, payload.length, remote.port, remote.address, error)
-        udp.send(payload2, 0, payload2.length, ann.request.port, ann.request.address, error)
-        console.log(`Announce ${ann.request.address}:${ann.request.port} -> ${remoteAddress}`)
-    }
+    if(typeof announcement[remoteAddress] === 'object')
+        announce(remote)
 
     if(msg.length === 0) return
     
@@ -57,8 +69,10 @@ udp.on('message', (msg, remote) => {
                     remote.port,
                     msg
                 ])
-            if(peer.key !== null) knownPeers[remoteAddress] = peer
-            return true
+            if(peer.key !== null){
+                knownPeers[remoteAddress] = peer
+                return true
+            }
         }
         if(new Date() - peer.lastAccess > __.LAST_ACCESS_LIMIT) return identifyPeer(true)
         peer = knownPeers[remoteAddress]
@@ -69,23 +83,34 @@ udp.on('message', (msg, remote) => {
     peer.lastAccess = new Date()
 
     message = peer.key.decrypt(msg)
+    let cmd = message[0]
+    message = message.slice(0,1)
 
     //Announcer
-    switch(message[0]){
+    switch(cmd){
         case '?':
             let remotePort = peer.key.encrypt(`${remote.port}`)
             udp.send(remotePort, 0, remotePort.length, remote.port, remote.address, error)
             return
         case '>':
-            message = message.slice(0,1)
             if(!IpRegex.test(message)) return
             if(typeof announcement[message] !== 'object')
                 announcement[message] = new Announcement()
-            /** @type {Announcement} */
+            
+                /** @type {Announcement} */
             let ann = announcement[message]
             ann.request.address = remote.address
             ann.request.port = remote.port
             console.log(`Request ${remoteAddress} -> ${message}`)
+
+            if(knownPeers[message] === 'object'){
+                let destinationAddress = message.split(':')
+                destinationAddress[1] = parseInt(destinationAddress[1])
+                announce({
+                    address: destinationAddress[0],
+                    port: destinationAddress[1]
+                })
+            }
             return
         default:
             message = Try(() => JSON.parse(message), null)
