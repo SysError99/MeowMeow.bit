972fe38aecfab943d3093379163cc36c49c336bf
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-02-16 02:12:35 +0700

    Improve code formatting

--- src/const.js
+++ src/const.js
@@ -4,10 +4,13 @@ const constants = {
     KEY: {
         LOCATION: 'key.server'
     },
+
     /** @type {number} Max amount of data accepted (default: 32 MB)*/
     MAX_PAYLOAD: 1048576 * 32,
+
     /** @type {number} Last access time (in millisecods, default: 1 day) */
     LAST_ACCESS_LIMIT: 1000 * 60 * 60 * 24,
+
     /** @type {boolean} If this is running in test mode */
     TEST: process.argv[2] === 'test' ? true : false
 }
