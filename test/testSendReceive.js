4bfd1c38e5b9c4f2a082db20f09f7b10aa251776
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-02-21 02:01:35 +0700

    Updated send text tester

--- test/testSendReceive.js
+++ test/testSendReceive.js
@@ -1,29 +1,13 @@
-const BaseN = require('../src/base.n')
 const Peer = require('../src/data/peer')
 const Receiver = require('../src/fn.receiver')
 
-const server = new Receiver((socket,key,data) => {
-
+const receiver = new Receiver((peer, data) => {
+    console.log(data.data)
 })
 
 let func = () => {
-
-    let peer = new Peer([
-        '::ffff:127.0.0.1',
-        parseInt(process.argv[2]),
-        BaseN.decode(server.key.current.get.pub(), '62').toString('base64')
-    ])
-
-    let msg = ['hello']
-
-    setInterval(async () => {
-        let result = await server.peer.send(peer, msg)
-        if(result.success)
-            console.log('hello ' + result.data[0])
-        else
-            console.error(result.message)
-    }, 1000)
-
+    let peer = new Peer('', 0, process.argv[2])
+    receiver.send(peer, 'hello')
 }
 
 func()
