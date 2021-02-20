d7238c64ed5c429d6ad991fa3787660142131dc2
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-02-21 04:01:52 +0700

    Fix test unit data type to array

--- test/testSendReceive.js
+++ test/testSendReceive.js
@@ -9,7 +9,7 @@ const receiver = new Receiver((peer, data) => {
 
 let func = () => {
     let peer = new Peer(['', 0, BaseN.decode(process.argv[2])])
-    receiver.send(peer, 'hello')
+    receiver.send(peer, ['hello'])
 }
 
 func()
