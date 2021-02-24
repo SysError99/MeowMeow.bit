efc27c4ec7ea8cebd063983ac9b7302e3201c044
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-02-25 03:04:21 +0700

    Post: removed unusable attributes, added hint

--- src/data/post.js
+++ src/data/post.js
@@ -9,16 +9,7 @@ const Post = function(d){
     /** @type {boolean} This is 'Post' object*/
     this.isPost = true
 
-    /** @type {PostPointer[]} Post comments*/
-    this.comment = []
-
-    /** @type {string} People names who like this post*/
-    this.like = []
-
-    /** @type {string} People signatures who like this post*/
-    this.likeSignature = []
-
-    /** @type {string[]} Base64-based media files*/
+    /** @type {string[]} List of media hashes*/
     this.media = []
 
     /** @type {PostPointer[]} Post mention*/
@@ -37,54 +28,50 @@ const Post = function(d){
     this.text = ''
 
     /**
+     * Other elements including:
+     * - Media
+     * - Likes
+     * - Comments
+     * - Shares
+     * will be separeted as files.
+     * 
+     * For example:
+     * ./<owner>.<post-number>.media.<n>.json
+     * ./<owner>.<post-number>.like.<n>.json
+     * ./<owner>.<post-number>.comment.<n>.json
+     * ./<owner>.<post-number>.share.<n>.json
+     * 
+     * Media hashes will be included and signed, others are not.
+     */
+
+    /**
      * Import d to object
      */
     let _import = () => {
-        if(Array.isArray(d[0])){
-            d[0].forEach(el => {
-                if(Array.isArray(el))
-                    self.comment.push(new PostPointer(el))
-            })
-        }
-        
+        if(Array.isArray(d[0]))
+            self.media = d[0]
+
         if(Array.isArray(d[1]))
-            self.like = d[1]
+            self.mention = d[1]
 
-        if(Array.isArray(d[2]))
-            self.likeSignature = d[2]
+        if(typeof d[2] === 'string')
+            self.owner = d[2]
 
-        if(Array.isArray(d[3]))
-            self.media = d[3]
+        if(typeof d[3] === 'string')
+            self.signature = d[3]
 
         if(Array.isArray(d[4]))
-            self.mention = d[4]
+            self.tag = d[4]
 
         if(typeof d[5] === 'string')
-            self.owner = d[5]
-
-        if(typeof d[6] === 'string')
-            self.signature = d[6]
-
-        if(Array.isArray(d[7]))
-            self.tag = d[7]
-
-        if(typeof d[8] === 'string')
-            self.text = d[8]
+            self.text = d[5]
     }
     /**
      * Export to array
      * @returns {Array} Array object
      */
     this.export = () => {
-        /** @type {PostPointer[]}*/
-        let comments = []
-        self.comment.forEach(el => {
-            comments.push(el.export())
-        })
         return [
-            comments,
-            self.like,
-            self.likeSignature,
             self.media,
             self.mention,
             self.owner,
