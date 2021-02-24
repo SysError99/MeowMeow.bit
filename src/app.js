30ba4ff309c29d2f356ff0df30674ec74c81388b
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-02-25 03:19:10 +0700

    App: added foundation of receiver functions

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@
 const Receiver = require('./fn.receiver')
 const Web = require('./fn.web')
 
+/**
+ * Stringify JSON object or array
+ * @param {Object} o JSON or array object to be stringified
+ * @returns {string}
+ */
+const str = o => JSON.stringify(o)
+
 /** HTTP web front-end app object*/
 const app = new Web()
 app.get('/', (req,res) => {
@@ -25,4 +32,45 @@ app.get('/find/:id', (req,res) => {
 /** Receiver Object*/
 const receiver = new Receiver((peer, data) => {
     console.log(`${data.data}`)
+    /**
+     * [0]:string Requester (poster) public key
+     * [1]:number Post position
+     * [2]:string Social Command
+     * :
+     * [.]:any
+     * [n]:string Signature (optional)
+     */
+
+    if( typeof data[0] !== 'string' ||
+        typeof data[1] !== 'number' ||
+        typeof data[2] !== 'string' )
+        return
+
+    if(!receiver.storage.access(`${data[0]}`)) // !account exists
+        return
+
+    switch(data[2]){
+
+        case 'media':
+            /**
+             * UNSTABLE, NOT TESTED
+             * 
+             * [3]:number media number
+             */
+            if(typeof data[3] !== 'number')
+                return
+            
+            let location = `${data[0]}.${data[1]}.media.${data[3]}`
+
+            if(receiver.storage.access(location))
+                return
+            
+            if(peer.mediaStream !== null)
+                return
+
+            peer.mediaStreamLocation = location
+            peer.mediaStream = receiver.storage.writeStream(location)
+            return
+
+    }
 })
