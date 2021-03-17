425470763a55771c7c00fe46a4c701301fa4aba6
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-03-18 00:13:09 +0700

    Added timeline post fetching in HTML body

---
+++ src/web/js/post-fetch.js
@@ -0,0 +1,26 @@
+// let currentPostNumber = 0
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        timelineRetrieve()
+    }
+}
+
+/**
+ * Retrieve a post from timeline
+ */
+const timelineRetrieve = () => {
+    let request = new XMLHttpRequest()
+
+    request.onreadystatechange = e => {
+        if(request.readyState === request.DONE && request.status === 200) {
+            if(request.responseText.length > 0)
+                document.getElementById('body').innerHTML += request.responseText
+        }
+    }
+
+    request.open('GET', `/post/`)
+    request.send()
+}
+
+timelineRetrieve()
