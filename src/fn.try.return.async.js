e792058af9408cc3977158fc47f64317e636cb0b
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-04-28 06:34:49 +0700

    ReturnAsync: added

---
+++ src/fn.try.return.async.js
@@ -0,0 +1,30 @@
+const Debugger = require('./fn.debugger')
+
+const ignores = require('./const').TRY_CATCH_IGNORE
+
+/**
+ * Try to execute this function and return value back if success, return undefined or specified value if fail.
+ * @param {function} fn A function
+ * @param {any} errreturn What to return when error has occured
+ */
+const tryCatch = async (fn, errreturn) => {
+    if (typeof fn === 'function') {
+        try {
+            return await fn()
+        }
+        catch(e) {
+            let err = new Error(e)
+
+            for (let ignore of ignores) {
+                if (err.stack.indexOf(ignore) >= 0)
+                    return errreturn
+            }
+
+            Debugger.error(err.stack)
+            return errreturn
+        }
+    }
+    else
+        throw Error('tryCatch() expects function!')
+}
+module.exports = tryCatch
